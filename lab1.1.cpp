22e68d59a00fdb51de7e341fa02db450bbc6f3e0
ELGrapes228 <50810890+ELGrapes228@users.noreply.github.com>
2021-12-04 16:07:38 +0300

    Update lab1.1.cpp

--- lab1.1.cpp
+++ lab1.1.cpp
@@ -1,4 +1,3 @@
-//работа со структурами
 #include <iostream>
 
 struct students
@@ -24,26 +23,29 @@ void studentsoutput(int n, struct students student2[])	// функция выв�
 
 void sortofsudents(int n, struct students student3[])	//сортировка структуры
 {
-	std::string copyofstring;
-	for (int i = 0; i < n-1; i++)
+	students replacer;
+	for (int i = 0; i < n - 1; i++)
 	{
 		for (int j = 0; j < n - 1; j++)
 		{
-			if ((student3[j].surname == student3[j + 1].surname) && (student3[j].name < student3[j + 1].name))	//по имени, если фамилии одинаковы
+			if ((student3[j].surname == student3[j + 1].surname))	//по имени, если фамилии одинаковы
 			{
-				copyofstring = student3[j].surname;
-				student3[j].surname = student3[j + 1].surname;
-				student3[j + 1].surname = copyofstring;
-				copyofstring = student3[j].name;
-				student3[j].name = student3[j + 1].name;
-				student3[j + 1].name = copyofstring;
-			};
-			if (student3[j].surname < student3[j + 1].surname)	//сортировка по фамилии
+				if (student3[j].name > student3[j + 1].name)
+				{
+					replacer = student3[j];
+					student3[j] = student3[j + 1];
+					student3[j + 1] = replacer;
+				}
+			}
+			else
 			{
-				copyofstring = student3[j].surname;
-				student3[j].surname = student3[j + 1].surname;
-				student3[j + 1].surname = copyofstring;
-			};
+				if (student3[j].surname > student3[j + 1].surname)	//сортировка по фамилии
+				{
+					replacer = student3[j];
+					student3[j] = student3[j+1];
+					student3[j + 1] = replacer;
+				};
+			}
 		}
 	}
 };
@@ -58,7 +60,40 @@ int main()
 	}
 	while (n < 5);
 
+	//n = 5;
+
 	students* student = new students[n];
+
+	/*student[0].surname = "aaa";
+	student[0].name = "petr";
+	student[0].marks[0] = 1;
+	student[0].marks[1] = 2;
+	student[0].marks[2] = 3;
+
+	student[1].surname = "aaa";
+	student[1].name = "ivan";
+	student[1].marks[0] = 2;
+	student[1].marks[1] = 3;
+	student[1].marks[2] = 4;
+
+	student[2].surname = "a";
+	student[2].name = "ccd";
+	student[2].marks[0] = 3;
+	student[2].marks[1] = 4;
+	student[2].marks[2] = 5;
+
+	student[3].surname = "abc";
+	student[3].name = "gfd";
+	student[3].marks[0] = 8;
+	student[3].marks[1] = 4;
+	student[3].marks[2] = 3;
+
+	student[4].surname = "rhjkl";
+	student[4].name = "tgysad";
+	student[4].marks[0] = 1;
+	student[4].marks[1] = 9;
+	student[4].marks[2] = 2;*/
+
 
 	for (int i = 0; i < n; i++)		//ввод данных в структуру
 	{
