5bdcf95e8067e350c5f3553792d33a78b61a7e58
ELGrapes228 <50810890+ELGrapes228@users.noreply.github.com>
2021-12-04 14:06:17 +0300

    Add files via upload

---
+++ lab2.1.cpp
@@ -0,0 +1,13 @@
+﻿//Создать лямбда-выражение для вывода элементов вектора.
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    int n = 10;
+	std::vector<int> abc;
+    Fibonacci(n, abc);
+    std::transform(abc.begin(), abc.end(), abc.begin(), [](int i) { return i = rand() % 1000 + 1; });
+    std::for_each(abc.begin(), abc.end(), [](int i) { std::cout << i << std::endl; });
+}
