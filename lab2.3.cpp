b71781bd8708d52792c02ac61d3dbd7e00107684
ELGrapes228 <50810890+ELGrapes228@users.noreply.github.com>
2021-12-04 14:00:30 +0300

    Add files via upload

---
+++ lab2.3.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void output(std::vector<int>& abc)
+{
+    std::for_each(abc.begin(), abc.end(), [](int i) { std::cout << i << std::endl; });
+}
+
+
+void FibonacciCont(float n, float k, std::vector<int>& abc)    //дозаполняет вектор числами Фибоначчи
+{
+    for (int i = n - k; i < n; i++)
+    {
+        abc.push_back(abc[i - 1] + abc[i - 2]);
+    }
+}
+
+void FibonacciBegin(float n, std::vector<int>& abc)    //заполняет вектор начальными числами Фибоначчи
+{
+    abc.push_back(0);
+    abc.push_back(1);
+    FibonacciCont(n, n - 2, abc);
+}
+
+int main()
+{
+    float n = 9;
+    float k = 4;
+    std::vector<int> abc;
+    FibonacciBegin(n, abc);
+    output(abc);
+    std::cout << std::endl;
+    std::vector<int> abc2;
+    std::for_each(abc.begin() + k, abc.end(), [&abc2](int i) { abc2.push_back(i); });
+    output(abc2);
+    std::cout << std::endl;
+    FibonacciCont(n, k, abc2);
+    output(abc2);
+}
