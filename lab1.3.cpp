715926cb377bc3a49537221b80485b178f3e74a6
ELGrapes228 <50810890+ELGrapes228@users.noreply.github.com>
2021-12-07 20:16:40 +0300

    Add files via upload

--- lab1.3.cpp
+++ lab1.3.cpp
@@ -1,4 +1,5 @@
-﻿//работа с конструкторами
+﻿//работа с конструкторами:
+//задать векторы разными конструкторами, один отсортировать, а втором найти модульное произведение элементов после последнего максимального элемента
 #include <iostream>
 
 class Vector
@@ -6,13 +7,22 @@ class Vector
 private:
     int chisla[10];
 public:
-	Vector() : chisla{ 2, 7, 16, 32, 8, 3, 1, 5, 10, 11 }
+	Vector()
 	{
 		std::cout << "Constructor without parameters" << std::endl;
 	}
 	Vector(int x)
 	{
-		chisla[10] = { 6, 2, 1, 76, 123, 13, 8, 9, 3, 29 };
+		chisla[0] = x;
+		chisla[1] = 2;
+		chisla[2] = 1;
+		chisla[3] = 76;
+		chisla[4] = 123;
+		chisla[5] = 13;
+		chisla[6] = 8;
+		chisla[7] = 123;
+		chisla[8] = 3;
+		chisla[9] = 29;
 		std::cout << "Constructor No. 2" << std::endl;
 	}
 	void sort()	//сортировка
@@ -22,7 +32,7 @@ public:
 		{
 			for (int j = 0; j < 10 - 1; j++)
 			{
-				if (chisla[j] < chisla[j + 1])
+				if (chisla[j] > chisla[j + 1])
 				{
 					copy = chisla[j];
 					chisla[j] = chisla[j + 1];
@@ -31,6 +41,29 @@ public:
 			}
 		}
 	};
+	int task17()
+	{
+		int max = chisla[0];
+		int ogo = 1;
+		for (int i = 0; i < 10; i++)
+		{
+			if (chisla[i] >= max)
+			{
+				max = chisla[i];
+				ogo = 1;
+			}
+			else
+			{
+				if (chisla[i] < 0)
+					ogo = ogo * chisla[i] * (-1);
+				else
+					ogo = ogo * chisla[i];
+			}/*
+			std::cout << "max " << i << " is " << max << std::endl;
+			std::cout << "ogo " << i << " is " << ogo << std::endl;*/
+		}
+		return ogo;
+	}
 	~Vector()
 	{
 		std::cout << "Memory has been cleaned. Good bye." << std::endl;
@@ -39,22 +72,45 @@ public:
 	{
 		return chisla[i];
 	}
+	void set_vector(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j)
+	{
+		chisla[0] = a;
+		chisla[1] = b;
+		chisla[2] = c;
+		chisla[3] = d;
+		chisla[4] = e;
+		chisla[5] = f;
+		chisla[6] = g;
+		chisla[7] = h;
+		chisla[8] = i;
+		chisla[9] = j;
+	};
 	void get_vector()
 	{
 		int i;
+		//std::cout << "----------------------------------------" << std::endl;
 		for (i = 0; i < 10; i++)
 		{
-			std::cout << chisla[i] << std::endl;
+			if (i + 1 == 10)
+				std::cout << chisla[i] << std::endl;
+			else
+				std::cout << chisla[i] << ", ";
 		}
+		//std::cout << "----------------------------------------" << std::endl;
+		std::cout << std::endl;
 	}
 };
 
 int main()
 {
 	Vector object1;
-	Vector object2 (2);
+	object1.set_vector(2, 7, 16, 32, 8, 3, 1, 5, 10, 11);
+	Vector object2 (19);
 	object1.get_vector();
-	std::cout << std::endl;
+	object1.sort();
+	object1.get_vector();
 	object2.get_vector();
+	std::cout << object2.task17();
+	std::cout << std::endl;
 	return 0;
 }
