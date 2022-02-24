86b7ea2a06b0dba71521758e8481dc26150418b1
ELGrapes228 <50810890+ELGrapes228@users.noreply.github.com>
2022-02-24 23:54:21 +0300

    Add files via upload

---
+++ lab8.1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+class Simple
+{
+public:
+    Simple() {}
+    operator bool() const 
+    {
+        return true;
+    }
+};
+
+class SimpleExplicit
+{
+public:
+    explicit SimpleExplicit() {}
+    explicit operator bool() const
+    {
+        return true;
+    }
+};
+
+int main()
+{
+    Simple bruh1();
+    SimpleExplicit bruh2();
+
+
+}
