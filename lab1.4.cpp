c251d0a0a837c6d57a216078b85484279b5537e3
ELGrapes228 <50810890+ELGrapes228@users.noreply.github.com>
2022-02-24 23:53:25 +0300

    Add files via upload

---
+++ lab1.4.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+class Massive	// дружественные функции, вариант 17: Определить количество строк, не содержащих ни одного нулевого элемента
+{
+private:
+    int a;
+    int b;
+    int **ab;
+public:
+    Massive(int aa, int bb) : a(aa), b(bb)
+    {
+        ab = new int*[a];
+        for (int i = 0; i < a; i++)
+        {
+            ab[i] = new int[b];
+            for (int j = 0; j < b; j++)
+            {
+                std::cin >> ab[a][b];
+                std::cout << std::endl;
+            }
+        }
+    };
+    ~Massive()
+    {
+        delete ab;
+    };
+    void output()
+    {
+        std::cout << std::endl;
+        for (int i = 0; i < a; i++)
+        {
+            for (int j = 0; j < b; j++)
+            {
+                std::cout << ab[i][j] <<"\t";
+            }
+            std::cout << std::endl;
+        }
+    };
+};
+
+
+int main()
+{
+    Massive abc(2, 3);
+    
+}
